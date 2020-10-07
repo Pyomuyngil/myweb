68e0acf8f7369d7e30e4621148c63d0147974440
Pyomuyngil <muyngil@gmail.com>
2020-10-07 18:36:08 +0900

    jjjkjkj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import {Image} from 'semantic-ui-react'
+import React from "react";
+import { Grid, Button, Image, Divider, Header, Icon } from "semantic-ui-react";
 
-import md from "./마당이.jpg"
-import Comments from './Comment.js'
-
+import md from "./마당이.jpg";
+import Comments from "./Comment.js";
+import MenuExampleBasic from "./Menu.js";
 
 function App() {
   return (
     <div>
-    <Image src={md} centered/>
-    <Comments />
+      <Grid centered>
+        <Grid.Row>
+          <Image src={md} centered />
+        </Grid.Row>
+        <Grid.Row>
+          <Button
+            color="red"
+            content="Like"
+            icon="heart"
+            label={{
+              basic: true,
+              color: "red",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+          <Button
+            basic
+            color="blue"
+            content="Share"
+            icon="fork"
+            label={{
+              as: "a",
+              basic: true,
+              color: "blue",
+              pointing: "left",
+              content: "0",
+            }}
+          />
+        </Grid.Row>
+      </Grid>
+
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="comment alternate" />
+          댓글을 달아주세요!
+        </Header>
+      </Divider>
+      <Comments />
+      <Divider horizontal>
+        <Header as="h4">
+          <Icon name="micro chip" />
+          Contack me!
+        </Header>
+      </Divider>
+      <br />
+      <div>
+        <Grid centered>
+          <Button circular color="facebook" icon="facebook" />
+          <Button circular color="twitter" icon="twitter" />
+          <Button circular color="linkedin" icon="linkedin" />
+          <Button circular color="google plus" icon="google plus" />
+        </Grid>
+      </div>
+      <MenuExampleBasic />
     </div>
   );
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,42 +1,69 @@
-import React from 'react';
-import {Image, Comment, Form, Button, Header} from 'semantic-ui-react'
+import React from "react";
+import { Icon, Image, Comment, Form, Button, Header } from "semantic-ui-react";
 
+import human from "./human.png";
 
-import human from "./human.png"
-class Comments extends React.Component{
-
-  render(){
-    return(
-
-      <Comment.Group>
-     <Header as='h3' dividing>
-       Comments
-     </Header>
-
-     <Comment>
-       <Comment.Avatar src={human} />
-       <Comment.Content>
-         <Comment.Author as='a'>Matt</Comment.Author>
-         <Comment.Metadata>
-           <div>Today at 5:42PM</div>
-         </Comment.Metadata>
-         <Comment.Text>How artistic!</Comment.Text>
-         <Comment.Actions>
-           <Comment.Action>Reply</Comment.Action>
-         </Comment.Actions>
-       </Comment.Content>
-     </Comment>
-
-
-     <Form reply>
-       <Form.TextArea />
-       <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-     </Form>
-   </Comment.Group>
-  )
-
-  }
+function SingleComment(detail) {
+  return (
+    <Comment>
+      <Comment.Avatar src={human} />
+      <Comment.Content>
+        <Comment.Author as="a">Matt</Comment.Author>
+        <Comment.Metadata>
+          <div>Today at 5:42PM</div>
+        </Comment.Metadata>
+        <Comment.Text>{detail.content}</Comment.Text>
+      </Comment.Content>
+    </Comment>
+  );
 }
 
+class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputContent: "",
+      commentsList: [],
+    };
+  }
+
+  render() {
+    console.log(this.state.commentsList);
+    return (
+      <Comment.Group style={{ marginLeft: "300px" }}>
+        <Header as="h3" dividing>
+          Comments
+        </Header>
+
+        {this.state.commentsList.map(comments => <SingleComment content = {comments}/>)}
+        <SingleComment />
+
+        <Form reply>
+          <Form.TextArea
+            value={this.state.inputContent}
+            placeholder="댓글을 입력해주세요"
+            onChange={(e) => this.setState({ inputContent: e.target.value })}
+          />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+            onClick={() =>
+              this.setState((prevState) => {
+                return {
+                  commentsList: [
+                    ...prevState.commentsList,
+                    this.state.inputContent,
+                  ],
+                };
+              })
+            }
+          />
+        </Form>
+      </Comment.Group>
+    );
+  }
+}
 
 export default Comments;
